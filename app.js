acc6ad607cc6a7d693b3ead588c21ad763e5053a
Chris Hernandez <HernandezMirandaChris@gmail.com>
2024-04-25 22:19:29 -0400

    Added index to main route as well as set up ejs and static files

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 // Import required modules
 const express = require('express');
+const path = require('path');
 
 //Create an instance of the Express application
 const app = express();
+
+// Set views directory for EJS Files
+app.set('views', path.join(__dirname,'views'));
+
+// Set EJS as the view engine
+app.set('view engine','ejs');
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname,'public')));
+
 
 // Define port number
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    res.send('Welcome to the soccer management system');
+    res.render('index');
 });
 
 //Start the server
